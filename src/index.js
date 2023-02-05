d0589302a7670480e9ebec5f45b913869836d679
calebdeji <calebdeji06@gmail.com>
2023-02-05 16:24:33 +0100

    refactor: update index

--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ const express = require('express');
 const configs = require('./configs/run');
 const routes = require('./routes');
 
-configs.runConfigs();
+function main() {
+  configs.runConfigs();
 
-const app = express();
+  const app = express();
 
-app.use('/voters', routes.VotersRouter);
-app.use('/elections', routes.ElectionRouter);
+  app.use('/voters', routes.VotersRouter);
+  app.use('/elections', routes.ElectionRouter);
 
-app.use('*', (req, res) => {
-  return res.status(404).json({ success: false, message: 'Unknown route' });
-});
+  app.use('*', (req, res) => {
+    return res.status(404).json({ success: false, message: 'Unknown route' });
+  });
 
-const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`App listening on ${PORT} port`);
-});
+  app.listen(PORT, () => {
+    console.log(`App listening on ${PORT} port`);
+  });
+}
 
-module.exports = app;
+main();
